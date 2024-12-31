89e77d652bcee907d17143faabf8f92ec99416de
tiagotaq <101513606+tiagotaq@users.noreply.github.com>
2024-12-31 13:01:17 -0300

    Adicionando instrução para subir artefatos

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
     vus: 100,
@@ -9,7 +10,7 @@ export const options = {
     }
 }
 
-export default function(){
+export default function () {
     const BASE_URL = 'https://test-api.k6.io/public/crocodiles/';
 
     const res = http.get(BASE_URL);
@@ -17,4 +18,10 @@ export default function(){
     check(res, {
         'status code 200': (r) => r.status === 200
     });
+}
+
+export function handleSummary(data) {
+    return {
+        "index.html": htmlReport(data),
+    };
 }
